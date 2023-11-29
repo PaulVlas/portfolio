94ba3291d91a4473ac742063856d47756e690e9d
PaulVlas <paul.vlaswinkel@gmail.com>
2023-11-29 11:18:06 +0000

    feat: :sparkles: Styling updates

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,8 +4,46 @@ export default {
 	theme: {
 		extend: {},
 	},
-	plugins: [require("daisyui")],
+	plugins: [require("daisyui"), require('@tailwindcss/typography')],
 	daisyui: {
-    themes: ["light", "dark"],
+    themes: [{
+        light: {
+          ...require("daisyui/src/theming/themes")["light"],
+					".hero-1": {
+						"color": "#1962d7"
+					},
+					".hero-2": {
+						"color": "#1b4ab1"
+					},
+					".hero-3": {
+						"color": "#16338c"
+					},
+					".hero-4": {
+						"color": "#0b1d69"
+					},
+					".hero-5": {
+						"color": "#000748"
+					},
+        },
+      },{
+        dark: {
+          ...require("daisyui/src/theming/themes")["dark"],
+					".hero-1": {
+						"color": "#506fd6"
+					},
+					".hero-2": {
+						"color": "#325dad"
+					},
+					".hero-3": {
+						"color": "#1f4b85"
+					},
+					".hero-4": {
+						"color": "#16385d"
+					},
+					".hero-5": {
+						"color": "#142538"
+					},
+        },
+      }],
   },
 }
