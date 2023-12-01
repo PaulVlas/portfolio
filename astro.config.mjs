c1fdcc71c1f9cabf0e28aa467c7a74ce20f10deb
PaulVlas <paul.vlaswinkel@gmail.com>
2023-12-01 10:15:05 +0000

    feat: :mag: Add robots and site index

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import robotsTxt from "astro-robots-txt";
+import { loadEnv } from "vite";
+
+const { SITE_URL } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: SITE_URL,
+  integrations: [tailwind(), sitemap(), robotsTxt()],
   prefetch: {
     prefetchAll: true
   }
